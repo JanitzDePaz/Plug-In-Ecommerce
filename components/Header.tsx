255865947f77697a14dc2f174ac6ada0024ea480
JanitzDePaz <janitzdp@gmail.com>
2025-10-28 08:41:57 +0100

    Add PlugInLogo.svg to the public/Header directory

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,15 @@ import { menuStorage } from "stores/menuStore";
 
 export default function Header(){
 
-    const {openedMenu, openMenu, closeMenu} = menuStorage()
+    const {openedMenu, toggleMenu, closeMenu} = menuStorage()
     
     return(
         <header className="flex-between border-b-2 border-black max-h-[15vh] p-6">
             <img src="Header/PlugInLogo.svg" alt="Logo icon" />
             <nav className="hidden md:block">
-                <ul className="flex gap-5">
+                <ul className="flex md:gap-10 gap-5">
                     {headerItems.map((item, i) => (
-                        <li key={i}>
+                        <li key={i} className="hover:scale-110 duration-100">
                             <Link  className="text-[#142B4E]" to={item.to}>{item.label}</Link>
                         </li>
                     ))}
@@ -26,11 +26,19 @@ export default function Header(){
                 <button className="header-button">
                     <img src="Header/ShoppingCart.svg" alt="Shopping cart icon" />
                 </button>
-                <button className="block  md:hidden header-button" onClick={openMenu}>
-                    <img src="Header/HeaderMenu.svg" alt="MenuIcon"/>
-                </button>
+                {openedMenu
+                ?   <button className=" block  md:hidden header-button z-10" onClick={closeMenu}>
+                        <img src="Header/CloseMenu.svg" alt="CloseMenuIcon" className="w-6 h-6"/>
+                    </button> 
+                    
+                :   
+                    <button className="block  md:hidden header-button" onClick={toggleMenu}>
+                        <img src="Header/HeaderMenu.svg" alt="MenuIcon"/>
+                    </button>
+                }
+                   
             </div>
-            {openedMenu && <Menu />}
+            <Menu />
         </header>
     );
 }
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,7 +1,20 @@
+import { menuStorage } from "stores/menuStore";
+import { headerItems } from "constants/headerItems";
+import { Link } from "react-router";
+import clsx from "clsx";
+
 export default function Menu() {
+    const {openedMenu} = menuStorage()
+    console.log(openedMenu);
     return(
-        <div className="absolute top-0 left-0 h-90 w-full bg-[#1b1b1bfa]">
-            <h1>PRueba</h1>
-        </div>
+        <nav className={clsx("absolute top-0 left-0 h-full w-full bg-[#1b1b1bfa] md:hidden transform transition-transform duration-300", openedMenu ? "traslate-y-full" : "translate-y-[-100vh]")}>
+            <ul className="flex flex-col gap-5 p-10">
+                {headerItems.map((item, i) => (
+                    <li key={i}>
+                        <Link  className="text-white text-2xl" to={item.to}>{item.label}</Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     )
 }
