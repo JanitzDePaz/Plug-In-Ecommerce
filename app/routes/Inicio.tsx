7de3a45afb023a8ce5db076c753a294f5a7ea34a
JanitzDePaz <janitzdp@gmail.com>
2025-10-30 12:43:07 +0100

    feat: update dependencies and add new assets
    - Added @react-three/drei and @react-three/fiber for 3D rendering support.
    - Updated types for React and React DOM to the latest versions.
    - Added @types/three for TypeScript support with Three.js.
    - Updated TypeScript to version 5.9.3.
    - Added new SVG asset for headset gradient.
    - Added new GLB asset for headphone model.
    - Updated tsconfig to include types for React and React DOM.

--- app/routes/Inicio.tsx
+++ app/routes/Inicio.tsx
@@ -1,7 +1,18 @@
-export default function(){
-    return (
-        <section className="LandingHeadsetBG max-h-full">
-            
-        </section>
-    );
+import Headphone from "../../components/landingPage/Headphone";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Box } from "@react-three/drei";
+
+export default function () {
+  return (
+    <section className="LandingHeadsetBG max-h-full">
+      <Canvas>
+        <Suspense fallback={null}>
+            <Box position={[1, 2, 3]} rotation={[0, Math.PI / 4, 0]}>
+                <Headphone />
+            </Box>
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 }
