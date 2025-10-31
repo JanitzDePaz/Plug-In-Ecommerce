fab99afc6eb71841f22bb0415f9e8d323e799a53
Janitz_De_Paz <janitzdp@gmail.com>
2025-10-31 21:20:15 +0100

    feat: enhance landing page with new headset color options and improved layout

--- app/routes/Inicio.tsx
+++ app/routes/Inicio.tsx
@@ -1,29 +1,51 @@
 import Headphone from "../../components/landingPage/Headphone";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls} from "@react-three/drei";
 import { headsetColorControl } from "stores/menuStore";
+import { Link } from "react-router";
+import Tienda from "./Tienda";
 
 export default function () {
 
     const {changeColorBlack, changeColorWhite} = headsetColorControl()
   return (
-    <section className="LandingHeadsetBG max-h-full flex">
-        <div className="flex flex-col justify-center items-center">
-            <Canvas style={{ width: '50vw', height: '80vh' }} camera={{ position: [0, 0, 5] }}>
-                <Suspense fallback={null}>
-                    <ambientLight intensity={1}/>
-                    <directionalLight position={[20, 15, -5]} intensity={1} />
-                    <directionalLight position={[-20, 15, 5]} intensity={1} />
-                    <Headphone scale={0.9} rotation={[0, Math.PI / Math.sin(50), 0]}/>
-                    <OrbitControls enableZoom={false} />
-                </Suspense>
-            </Canvas>
-            <div className="px-3 py-2 rounded-full flex justify-center gap-2 bg-[#615E74]">
-                <button className="rounded-full bg-[#3B3939] w-10 h-10" onClick={changeColorBlack}></button>
-                <button className="rounded-full bg-[#cacaca] w-10 h-10" onClick={changeColorWhite}></button>
+    <section className="LandingHeadsetBG py-[5vh] flex flex-col">
+        <h1 className="landingTitle">Nuevo color disponible</h1>
+        <div className="flex justify-center gap-[10vw] flex-col lg:flex-row">
+            <div className="flex-center flex-col items-center h-[70vh]">
+                <Canvas style={{ width: '25vw', height: '70vh' }} camera={{ position: [0, 0, 4] }} frameloop="demand">
+                    <Suspense fallback={null}>
+                        <ambientLight intensity={1}/>
+                        <directionalLight position={[20, 15, -5]} intensity={1} />
+                        <directionalLight position={[-20, 15, 5]} intensity={1} />
+                        <Headphone scale={0.9} rotation={[0, Math.PI / Math.sin(50), 0]}/>
+                        <OrbitControls enableZoom={false} enableDamping={false} enablePan={false} enableRotate={false} autoRotate={true}/>
+                    </Suspense>
+                </Canvas>
+                <div className="px-3 py-2 rounded-full flex-center gap-2 bg-[#615E74]">
+                    <button className="rounded-full bg-[#A28C7F] w-10 h-10" onClick={changeColorBlack}></button>
+                    <button className="rounded-full bg-[#E8E6E6] w-10 h-10" onClick={changeColorWhite}></button>
+                </div>
+            </div>
+            <div className="w-[25vw] flex-center flex-col gap-10">
+                <h2 className="landingSubtitle">NeoSound</h2>
+                <p className="landingParagraph">
+                    Cada nota cobra vida con una nitidez que te envuelve. 
+                    Los graves potentes y los agudos definidos te hacen sentir la música tal y como fue creada. 
+                    No escuchas, vives la experiencia.
+                </p>
+                <div className="flex-center flex-col gap-5">
+                    <Link to={"/tienda"}>
+                        <button className="landingSecondaryButton">Leer más</button>
+                    </Link>
+                    <Link to={"/tienda"}>
+                        <button className="landingPrimaryButton">Comprar ahora</button>
+                    </Link>
+                </div>
             </div>
         </div>
+        
       
     </section>
   );
--- stores/menuStore.ts
+++ stores/menuStore.ts
@@ -1,22 +1,10 @@
 import { create } from 'zustand';
 
-type menuProp = {
-    openedMenu: boolean,
-    toggleMenu: () => void,
-    closeMenu: () => void
-}
-
 export const menuStorage = create<menuProp>((set) => ({
     openedMenu: false,
     toggleMenu: () => set((state) => ({openedMenu: !state.openedMenu})),
     closeMenu: () => set({openedMenu:false})
 }));
-
-type searchProp ={
-    searchInput: boolean,
-    toggleSearchInput: () => void,
-    outSearch: () => void
-}
 
 export const searchStorage = create<searchProp>((set) => ({
     searchInput: false,
@@ -24,28 +12,15 @@ export const searchStorage = create<searchProp>((set) => ({
     outSearch: () => ({searchInput: false})
 }))
 
-type cartProp = {
-    cartMenu: boolean,
-    toggleCart: () => void,
-    closeCart: () => void
-}
-
 export const cartStorage = create<cartProp>((set) => ({
     cartMenu: false,
     toggleCart: () => set((state) => ({cartMenu: !state.cartMenu})),
     closeCart: () => ({CartMenu: false})
 }))
 
-type HeadsetColorProp = {
-    headsetColor1: string,
-    headsetColor2: string,
-    changeColorBlack: () => void,
-    changeColorWhite: () => void
-}
-
 export const headsetColorControl = create<HeadsetColorProp>((set) => ({
-    headsetColor1: "#615E74",
-    headsetColor2: "#3D3B3B",
-    changeColorBlack: () => set({headsetColor1: "#615E74", headsetColor2: "#3D3B3B"}),
-    changeColorWhite: () => set({headsetColor1: "#E8E6E6", headsetColor2: "#D6D6D6"})
+    headsetColor1: "#A28C7F",
+    headsetColor2: "#D6C4B0",
+    changeColorBlack: () => set({headsetColor1: "#A28C7F", headsetColor2: "#D6C4B0"}),
+    changeColorWhite: () => set({headsetColor1: "#E8E6E6", headsetColor2: "#D6D2CC"})
 }))
