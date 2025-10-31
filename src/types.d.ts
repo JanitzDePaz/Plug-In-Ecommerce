44941464fd5d0e64c7be412979b8c71777b03e08
Janitz_De_Paz <janitzdp@gmail.com>
2025-10-31 21:20:02 +0100

    feat: change types to types.d.ts file

---
+++ src/types.d.ts
@@ -0,0 +1,25 @@
+type menuProp = {
+    openedMenu: boolean,
+    toggleMenu: () => void,
+    closeMenu: () => void
+}
+
+type searchProp ={
+    searchInput: boolean,
+    toggleSearchInput: () => void,
+    outSearch: () => void
+}
+
+
+type cartProp = {
+    cartMenu: boolean,
+    toggleCart: () => void,
+    closeCart: () => void
+}
+
+type HeadsetColorProp = {
+    headsetColor1: string,
+    headsetColor2: string,
+    changeColorBlack: () => void,
+    changeColorWhite: () => void
+}
