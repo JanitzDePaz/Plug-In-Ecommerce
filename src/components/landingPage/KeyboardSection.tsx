e84427e5b569a845de08fc2340b43ca0f06f6475
Janitz_De_Paz <janitzdp@gmail.com>
2025-11-10 21:04:59 +0100

    refactor: enhance KeyboardSection with text comparison and dynamic input handling

--- src/components/landingPage/KeyboardSection.tsx
+++ src/components/landingPage/KeyboardSection.tsx
@@ -1,8 +1,22 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import clsx from "clsx"
+import { texts } from "../../constants/typingText"
 
 export const KeyboardSection = () => {
+
     const [writedText, setWritedText] = useState(" ");
-    console.log(writedText)
+    const writeOnFinal = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        if (writeOnFinal.current) {
+        const len = writeOnFinal.current.value.length;
+        writeOnFinal.current.focus();
+        writeOnFinal.current.setSelectionRange(len, len);
+  }
+}, [writedText]);
+    
+    const writedTextCompare = writedText.split("")
+    const expectedText = texts[0].split("")
+
     return (
         <section className="max-h-screen flex-center gap-30 items-center bg-amber-600">
             <div className="">
@@ -17,16 +31,14 @@ export const KeyboardSection = () => {
                 </div>
             </div>
             <div className="w-[40vw] min-h-[70vh] flex relative">
-                <input type="text" className="absolute opacity-0 h-full w-full" onChange={(e) => setWritedText(e.target.value)}/>
-                <div className="flex">
-                    {writedText.split("").map((char, index) => (
-                        char === " "
-                        ?
-                        
-                        :
-                        }
-
-                        <p key={index} className="text-4xl font-mono">{char === " " ? "\u00A0" : char}</p>
+                <input type="text" className="absolute opacity-0 h-full w-full" ref={writeOnFinal} onChange={(e) => setWritedText(e.target.value)}/>
+                <div className="flex flex-wrap gap-6 h-fit p-10">
+                    {writedText.split(" ").map((word, index) => (
+                        <div key={index} className="flex flex-wrap">
+                            {word.split("").map((char, charIndex) => (
+                                <p key={charIndex} className={clsx("text-4xl font-mono", writedTextCompare[charIndex]==expectedText[charIndex] ? "bg-green-500" : "bg-red-600")}>{char}</p>
+                            ))}
+                        </div>
                     ))}
                 </div>
             </div>
---
+++ src/constants/typingText.ts
@@ -0,0 +1,7 @@
+export const texts = [
+    "Hola, bienvenido a nuestra tienda en línea.",
+    "Explora nuestra amplia gama de productos.",
+    "Disfruta de ofertas exclusivas y descuentos.",
+    "Compra de manera segura y rápida.",
+    "Contáctanos para cualquier consulta o ayuda."
+]
