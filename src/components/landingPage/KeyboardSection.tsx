767219489ca2b8c2d724f691b4c327a19be417f4
JanitzDePaz <janitzdp@gmail.com>
2025-11-13 18:28:47 +0100

    wip: Improve the typing test with spacing error

--- src/components/landingPage/KeyboardSection.tsx
+++ src/components/landingPage/KeyboardSection.tsx
@@ -48,7 +48,7 @@ export const KeyboardSection = () => {
         </div>
       </div>
       <div className="w-[40vw] min-h-[70vh] flex relative">
-        <div className="flex flex-wrap gap-6 h-fit p-10">
+        <div className="flex flex-wrap gap-6 h-fit p-10 absolute">
           {writedText.map((word, index) =>
             <span className="flex" key={index}>
                 {word.split("").map((char, i) => (
@@ -61,15 +61,29 @@ export const KeyboardSection = () => {
           {currentWord.length > 0 
             ? 
                 <span className="flex">
-                    {currentWord.split("").map((char, i) => (
-                        <p className={clsx("text-2xl", char === expectedText[i] ? "text-green-500" : "text-black")} key={i}>
+                    {currentWord.split("").map((char, i) => {
+                    const compareNumber = writedText.join("").length
+                    return (
+                        <p className={clsx("text-2xl", char === expectedText[compareNumber+i] ? "text-green-500" : "text-black")} key={i}>
                             {char}
                         </p>
-                    ))}
+                    )})}
                 </span>
             :
                 null
             }
+        </div>
+
+        <div className="flex flex-wrap gap-6 h-fit p-10 absolute">
+          {expectedText.map((word, index) =>
+            <span className="flex" key={index}>
+                {word.split("").map((char, i) => (
+                    <p className="text-gray-400 text-2xl" key={i}>
+                        {char}
+                    </p>
+                ))}
+            </span>
+          )}
         </div>
       </div>
     </section>
