7f804d847f1b8d9cd29e2b57344432c5879a9787
JanitzDePaz <janitzdp@gmail.com>
2025-11-14 12:15:52 +0100

    refactor: change the typing test logic

--- src/components/landingPage/KeyboardSection.tsx
+++ src/components/landingPage/KeyboardSection.tsx
@@ -1,27 +1,19 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 import { texts } from "../../constants/typingText";
 
-let wordCount = 0;
+  const stringToCompare = texts[Math.floor(Math.random()*10+1)]
+  const screenText = stringToCompare
+
 export const KeyboardSection = () => {
-    const [currentWord, setCurrentWord] = useState<string>("");
-    const [writedText, setWritedText] = useState<string[]>([]);
+    const [writedText, setWritedText] = useState <String>("")
 
   useEffect(() => {
     const handleKeydown = (event: KeyboardEvent) => {
-      console.log(event.key);
-      if (event.key == " ") {
-        setCurrentWord(prev => {
-          const word = prev.trim();
-          if (word) setWritedText(arr => [...arr, word]);
-          return "";
-        });
-
-      }
        if (/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]$/.test(event.key)) {
-        setCurrentWord(prev => prev + event.key);
+        setWritedText(prev => prev + event.key);
       } else if (event.key === "Backspace") {
-        setCurrentWord(prev => prev.slice(0, -1));
+        setWritedText(prev => prev.slice(0, -1));
       }
     };
 
@@ -30,9 +22,7 @@ export const KeyboardSection = () => {
     return () => {
       removeEventListener("keydown", handleKeydown);
     };
-  }, [currentWord]);
-
-  const expectedText = texts[0].split(" ");
+  }, []);
 
   return (
     <section className="max-h-screen flex-center gap-30 items-center bg-amber-600">
@@ -49,41 +39,19 @@ export const KeyboardSection = () => {
       </div>
       <div className="w-[40vw] min-h-[70vh] flex relative">
         <div className="flex flex-wrap gap-6 h-fit p-10 absolute">
-          {writedText.map((word, index) =>
-            <span className="flex" key={index}>
-                {word.split("").map((char, i) => (
-                    <p className={clsx("text-2xl", char === expectedText[i] ? "text-green-500" : "text-black")} key={i}>
-                        {char}
-                    </p>
-                ))}
-            </span>
-          )}
-          {currentWord.length > 0 
-            ? 
-                <span className="flex">
-                    {currentWord.split("").map((char, i) => {
-                    const compareNumber = writedText.join("").length
-                    return (
-                        <p className={clsx("text-2xl", char === expectedText[compareNumber+i] ? "text-green-500" : "text-black")} key={i}>
-                            {char}
-                        </p>
-                    )})}
-                </span>
-            :
-                null
-            }
-        </div>
+          //Hacer el typing test con el mismo array siempre
+          {
+            screenText.split(" ").map((word, wordIndex) =>(
+              <div key={wordIndex} className="flex">
+                  {
+                    word.split("").map((char, charIndex) => (
+                      <p>{char}</p>
+                    ))
+                  }
+              </div>
+            ))
 
-        <div className="flex flex-wrap gap-6 h-fit p-10 absolute">
-          {expectedText.map((word, index) =>
-            <span className="flex" key={index}>
-                {word.split("").map((char, i) => (
-                    <p className="text-gray-400 text-2xl" key={i}>
-                        {char}
-                    </p>
-                ))}
-            </span>
-          )}
+          }
         </div>
       </div>
     </section>
--- src/constants/typingText.ts
+++ src/constants/typingText.ts
@@ -3,5 +3,10 @@ export const texts = [
     "Explora nuestra amplia gama de productos.",
     "Disfruta de ofertas exclusivas y descuentos.",
     "Compra de manera segura y rápida.",
+    "Contáctanos para cualquier consulta o ayuda.",
+    "Hola, bienvenido a nuestra tienda en línea.",
+    "Explora nuestra amplia gama de productos.",
+    "Disfruta de ofertas exclusivas y descuentos.",
+    "Compra de manera segura y rápida.",
     "Contáctanos para cualquier consulta o ayuda."
 ]
