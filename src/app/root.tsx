ec1e2714cc47120b9c20a0b593282fc7c1ee9fe8
JanitzDePaz <janitzdp@gmail.com>
2025-11-21 12:06:36 +0100

    refactor: wrap children in main with flex-1 to push Footer to bottom

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -23,9 +23,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="flex flex-col min-h-screen">
         <Header />
-        {children}
+        <main className="flex-1">
+          {children}
+        </main>
         <Footer />
         <ScrollRestoration />
         <Scripts />
