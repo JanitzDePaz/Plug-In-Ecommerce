73823d8e78d1005e34b74ba3ddabfc49e5d7201f
JanitzDePaz <janitzdp@gmail.com>
2025-11-24 12:51:02 +0100

    feat: create Product class with constructor and basic attributes

---
+++ server/src/main/java/com/plugin/server/model/Product.java
@@ -0,0 +1,23 @@
+package com.plugin.server.model;
+
+public class Product {
+    
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private String imgURL;
+    private boolean active;
+
+
+    public Product(int id, String name, String description, double price, String category, String imgURL, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.imgURL = imgURL;
+        this.active = active;
+    }
+}
