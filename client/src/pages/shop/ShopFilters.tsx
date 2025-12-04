6979b6406a347bac8791338ad75ccb0ea5c65934
JanitzDePaz <janitzdp@gmail.com>
2025-12-04 10:15:26 +0100

    feat(ui): add price range filter layout

--- client/src/pages/shop/ShopFilters.tsx
+++ client/src/pages/shop/ShopFilters.tsx
@@ -3,8 +3,20 @@ import clsx from "clsx"
 export const ShopFilters = () => {
 
     return(
-        <aside className=" px-20 border">
+        <aside className="max-w-3/10 border">
             <h3 className="text-center text-2xl">Filtros</h3>
+            <div className="flex-center flex-col px-5">
+                <div className="flex justify-between">
+                    <label htmlFor="minPrice" className="w-20 text-xs">Min price</label>
+                    <label htmlFor="maxPrice" className="w-20 text-right text-xs">Max price</label>
+                </div>
+                <div className="flex justify-between gap-2 items-center ">
+                    <input type="number" id="minPrice" placeholder="0" className="border w-20 no-spinner text-center"/>
+                    <hr className="w-5"/>                      
+                    <input type="number" id="maxPrice"  placeholder="2000" className="border w-20 no-spinner text-center"/>
+                </div>  
+            </div>
+            
         </aside>
     )
 }
