c88ab8797f2f10372041128a5a01afc2fb5e82f8
Janitz_De_Paz <janitzdp@gmail.com>
2025-12-07 21:09:24 +0100

    feat: Implement product fetching and display on ProductPage

--- client/src/app/routes/shop/ProductPage.tsx
+++ client/src/app/routes/shop/ProductPage.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { getProducts } from "src/api/getProducts";
 
 export default function ProductPage(){
     const { productName } = useParams();
+    const [ product, setProduct ] = useState<productProp | null>(null);
 
-    if(!productName){
-        return <div>No se ha encontrado el producto</div>
-    }
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const products: productProp[] = await getProducts();
+            const foundProduct = products.find((prod) => prod.name === productName);
+            setProduct(foundProduct || null);
+        }
+
+        fetchProduct();
+    }, [productName]);
     return(
-        <div>{productName}</div>
-)}
+        <div>
+             <div>{product?.name}</div>
+            <div>{product?.price}</div>
+            <div>{product?.rate}</div>
+            <div>{product?.category}</div>
+            <div>{product?.deliveryDays}</div>
+        </div>
+       
+    )
+}
