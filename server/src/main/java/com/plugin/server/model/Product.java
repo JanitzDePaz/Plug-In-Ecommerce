403d9ed64b9ed6763d80d00964222b5b16a4fd58
Janitz_De_Paz <janitzdp@gmail.com>
2025-12-08 15:29:33 +0100

    feat: Refactor Product class to enhance attributes and constructor

--- server/src/main/java/com/plugin/server/model/Product.java
+++ server/src/main/java/com/plugin/server/model/Product.java
@@ -1,30 +1,44 @@
 package com.plugin.server.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Product {
 
     private int id;
     private String name;
+    private String slug;
     private String description;
-    private double price;
+    private String longDescription;
+    private BigDecimal price;
     private String category;
     private int rate;
-    private LocalDate deliveryDays;
-    private String imgUrl;
+    private LocalDate estimateDeliveryDays;
+    private ArrayList<String> imgUrl;
     private boolean active;
+    private int stock;
+    private String brand;
+    private int discount;
+    private ArrayList<String> specifications;
 
-    public Product(int id, String name, String description, double price, String category, String imgUrl,
-            boolean active) {
+    public Product(int id, String name, String description, String longDescription, BigDecimal price, String category, ArrayList<String> imgUrl,
+        int stock, String brand, int discount, ArrayList<String> specifications) {
         this.id = id;
         this.name = name;
+        this.slug = name.toLowerCase().replaceAll(" ", "-").replaceAll("[^a-z0-9\\-]", "");
         this.description = description;
+        this.longDescription = longDescription;
         this.price = price;
         this.category = category;
         this.rate = (int) (Math.random() * 10) + 1;
-        this.deliveryDays = LocalDate.now().plusDays((int) (Math.random() * 3) + 1);
+        this.estimateDeliveryDays = LocalDate.now().plusDays((int) (Math.random() * 3) + 1);
         this.imgUrl = imgUrl;
-        this.active = active;
+        this.stock = stock;
+        this.active = stock > 0;
+        this.brand = brand;
+        this.discount = discount;
+        this.specifications = specifications;
     }
 
     public int getId() {
@@ -39,7 +53,11 @@ public class Product {
         return description;
     }
 
-    public double getPrice() {
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public BigDecimal getPrice() {
         return price;
     }
 
@@ -52,14 +70,30 @@ public class Product {
     }
 
     public LocalDate getDeliveryDays() {
-        return deliveryDays;
+        return estimateDeliveryDays;
     }
 
-    public String getImgUrl() {
+    public ArrayList<String> getImgUrl() {
         return imgUrl;
     }
 
     public boolean getActive() {
         return active;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public int getDiscount() {
+        return discount;
+    }
+    public ArrayList<String> getSpecifications() {
+        return specifications;
+    }
+    public String getSlug() {
+        return slug;
     }
 }
