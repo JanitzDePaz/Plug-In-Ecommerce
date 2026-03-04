2200e272ca8fbb2dfe62622b07c5413739f27588
Janitz <janitzdp@gmail.com>
2026-03-04 22:34:12 +0100

    refactor(api): improve formatting and consistency in getProductDetails function

--- client/src/api/getProductDetails.ts
+++ client/src/api/getProductDetails.ts
@@ -1,32 +1,31 @@
-export const getProductDetails = async (productSlug : string) => {
-    const res = await fetch(`http://localhost:8080/products/${productSlug}`)
+export const getProductDetails = async (productSlug: string) => {
+  const res = await fetch(`http://localhost:8080/products/${productSlug}`);
 
-    if(!res.ok){
-        throw new Error(`HTTP error! status: ${res.status}`);
-    }
-    const productDetails = await res.json()
-    const deliveryDate = new Date(productDetails.estimateDeliveryDays)
-    
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+  const productDetails = await res.json();
+  const deliveryDate = new Date(productDetails.estimateDeliveryDays);
 
-    const cleanProductDetails : ShowProductDetailsProp = {
-        id: productDetails.id,
-        slug: productDetails.slug,
-        name: productDetails.name,
-        description: productDetails.description,
-        longDescription: productDetails.longDescription,
-        price: productDetails.price,
-        category: productDetails.category,
-        rate: productDetails.rate,
-        estimatedDate: deliveryDate,
-        dayName: deliveryDate.toLocaleDateString("es-ES", {weekday: "long"}),
-        monthName: deliveryDate.toLocaleDateString("es-ES", {month: "long"}),
-        imgUrl: productDetails.imgUrl,
-        mainImg: productDetails.mainImg,
-        active: productDetails.active,
-        stock: productDetails.stock,
-        brand: productDetails.brand,
-        discount: productDetails.discount,
-        specifications: productDetails.specifications,
-    }
-    return cleanProductDetails
-}
+  const cleanProductDetails: ShowProductDetailsProp = {
+    id: productDetails.id,
+    slug: productDetails.slug,
+    name: productDetails.name,
+    description: productDetails.description,
+    longDescription: productDetails.longDescription,
+    price: productDetails.price,
+    category: productDetails.category,
+    rate: productDetails.rate,
+    estimatedDate: deliveryDate,
+    dayName: deliveryDate.toLocaleDateString("es-ES", { weekday: "long" }),
+    monthName: deliveryDate.toLocaleDateString("es-ES", { month: "long" }),
+    imgUrl: productDetails.images,
+    mainImg: productDetails.mainImage,
+    active: productDetails.active,
+    stock: productDetails.stock,
+    brand: productDetails.brand,
+    discount: productDetails.discount,
+    specifications: productDetails.specifications,
+  };
+  return cleanProductDetails;
+};
