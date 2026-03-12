8257423fd461e4f5387f2eefcba8cdda932d8285
Janitz <janitzdp@gmail.com>
2026-03-12 12:05:58 +0100

    feat(shop): implement responsive category filters

--- client/src/app/routes/shop/Tienda.tsx
+++ client/src/app/routes/shop/Tienda.tsx
@@ -1,24 +1,20 @@
-import { ShopTopFilters } from "src/pages/shop/ShopTopFilters";
 import { ShopFilters } from "src/pages/shop/ShopFilters";
 import { ShopMain } from "src/pages/shop/ShopMain";
 import { MobileFilters } from "src/pages/shop/MobileFilters";
-import clsx from "clsx";
 import { mobileShopState } from "src/stores/shopStore";
+import { ShopTopFilters } from "src/pages/shop/ShopTopFilters";
 
 export default function Tienda() {
-  const activeMobileShop = mobileShopState((e) => e.activeMobileShop)
-  const setMobileShopState = mobileShopState((e) => e.setMobileShopState)
+  const activeMobileShop = mobileShopState((e) => e.activeMobileShop);
+  const setMobileShopState = mobileShopState((e) => e.setMobileShopState);
   return (
-    <main className="flex-center md:flex-row gap-10 lg:gap-20 w-[95vw] md:w-[90vw] lg:w-[85vw] py-10 md:py-15 m-auto relative">
-      
-      <div className="hidden md:block w-3xs">
-        <ShopFilters />
+    <main className="flex flex-col justify-center items-center">
+      <div className="hidden md:block">
+        <ShopTopFilters />
       </div>
-        
-        <ShopMain />
-        <button
+      <button
         onClick={() => setMobileShopState(true)}
-        className="md:hidden fixed left-2 z-40 bg-black text-white p-4 rounded-full shadow-2xl active:scale-95 transition-transform"
+        className="md:hidden fixed top-30 left-2 z-40 bg-black text-white p-4 rounded-full shadow-2xl active:scale-95 transition-transform"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -34,6 +30,16 @@ export default function Tienda() {
           <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
         </svg>
       </button>
+
+      <div className="flex-center md:flex-row gap-10 lg:gap-20 w-[95vw] md:w-[90vw] lg:w-[85vw] py-10 md:py-5">
+        <div className="hidden md:block w-3xs">
+          <ShopFilters />
+        </div>
+
+        <ShopMain />
+      </div>
+
+      
       <MobileFilters />
     </main>
   );
--- client/src/pages/shop/ShopTopFilters.tsx
+++ client/src/pages/shop/ShopTopFilters.tsx
@@ -4,16 +4,19 @@ export const ShopTopFilters = () => {
   const { setCategoryFilter: changeCategoryFilter } = categoryFilterStorage();
 
   return (
-    <nav className="flex justify-center gap-5 flex-wrap ">
-      {shopTopFilterItems.map((btn, i) => (
-        <button
-          key={i}
-          onClick={() => changeCategoryFilter(btn.filter)}
-          className="py-2 px-4 border border-gray-400 text-lg rounded-sm hover:bg-gray-200 hover:border-[#163461]"
-        >
-          {btn.name}
-        </button>
-      ))}
+    <nav className="flex-center gap-5 flex-col py-5">
+      <h3 className="text-center text-xl block md:hidden">Tipo</h3>
+      <div className="md:flex gap-2 md:flex-wrap md:justify-around grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))]">
+        {shopTopFilterItems.map((btn, i) => (
+          <button
+            key={i}
+            onClick={() => changeCategoryFilter(btn.filter)}
+            className=" py-2 px-4 border border-gray-400 text-lg rounded-sm hover:bg-gray-200 hover:border-[#163461]"
+          >
+            {btn.name}
+          </button>
+        ))}
+      </div>
     </nav>
   );
 };
