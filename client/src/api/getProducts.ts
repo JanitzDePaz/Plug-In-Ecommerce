adcb247bad84d1e18a1995306402477447ac666a
Janitz <janitzdp@gmail.com>
2026-03-14 23:38:03 +0100

    refactor(getProducts): streamline error handling and improve code readability

--- client/src/api/getProducts.ts
+++ client/src/api/getProducts.ts
@@ -1,38 +1,33 @@
 export const getProducts = async () => {
   const url = import.meta.env.VITE_API_URL;
-  try {
-    const res = await fetch(`${url}/products`);
+  const res = await fetch(`${url}/products`);
 
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-
-    const productsData = await res.json();
-
-    const cleanProducts: ProductCardsData[] = [];
-
-    productsData.forEach((prod: ProductCardPropDTO) => {
-      const deliveryDate = new Date(prod.estimateDeliveryDays);
-      const cleanProd = {
-        id: prod.id,
-        name: prod.name,
-        slug: prod.slug,
-        price: prod.price,
-        imgUrl: prod.imgUrl,
-        rate: prod.rate,
-        category: prod.category,
-        estimatedDate: deliveryDate,
-        dayName: deliveryDate.toLocaleDateString("es-ES", { weekday: "long" }),
-        monthName: deliveryDate.toLocaleDateString("es-ES", { month: "long" }),
-        active: prod.active,
-        discount: prod.discount,
-      };
-
-      cleanProducts.push(cleanProd);
-    });
-    return cleanProducts;
-  } catch (error) {
-    console.log(error);
-    return [];
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
   }
+
+  const productsData = await res.json();
+
+  const cleanProducts: ProductCardsData[] = [];
+
+  productsData.forEach((prod: ProductCardPropDTO) => {
+    const deliveryDate = new Date(prod.estimateDeliveryDays);
+    const cleanProd = {
+      id: prod.id,
+      name: prod.name,
+      slug: prod.slug,
+      price: prod.price,
+      imgUrl: prod.imgUrl,
+      rate: prod.rate,
+      category: prod.category,
+      estimatedDate: deliveryDate,
+      dayName: deliveryDate.toLocaleDateString("es-ES", { weekday: "long" }),
+      monthName: deliveryDate.toLocaleDateString("es-ES", { month: "long" }),
+      active: prod.active,
+      discount: prod.discount,
+    };
+
+    cleanProducts.push(cleanProd);
+  });
+  return cleanProducts;
 };
