eb0637a6416d45e0c07b96b5fac3efc8cd9cfc46
Janitz <janitzdp@gmail.com>
2026-03-18 09:42:29 +0100

    fix(CartMenu): add z-index to cart menu for proper stacking

--- client/src/components/layout/header/CartMenu.tsx
+++ client/src/components/layout/header/CartMenu.tsx
@@ -7,8 +7,8 @@ export const CartMenu = () => {
   return (
     <nav
       className={clsx(
-        "opacity-0 bg-white max-h-85vh] min-h-[70vh] overflow-y-scroll w-full md:min-w-[20vw] md:max-w-[30vw] absolute top-full right-0 border-2 border-black duration-200",
-        cartMenu ? "translate-y-0 opacity-100" : "translate-x-full opacity-0"
+        "opacity-0 bg-white max-h-85vh] min-h-[70vh] overflow-y-scroll w-full md:min-w-[20vw] md:max-w-[30vw] absolute top-full right-0 border-2 border-black duration-200 z-1000",
+        cartMenu ? "translate-y-0 opacity-100" : "translate-x-full opacity-0",
       )}
     ></nav>
   );
